ca6f93267c16ab6475bed6f6e3eb1b64fe39db28
Henry Falade <tofex4eva@yahoo.com>
2018-12-29 00:18:08 +0100

    Modify wp-env to allow non .env vars set

--- wp-env.php
+++ wp-env.php
@@ -1,38 +1,14 @@
 <?php
 $env_file = ABSPATH . '.env';
 
-if ( !file_exists ( $env_file ) ) {
-    define( 'WPINC', 'wp-includes' );
-    require_once( ABSPATH . WPINC . '/load.php' );
+if ( file_exists ( $env_file ) ) {
+    $contents = file_get_contents( $env_file );
+    $variables = explode( PHP_EOL, $contents );
+    $variables = array_diff($variables, [""]);
 
-    // Standardize $_SERVER variables across setups.
-    wp_fix_server_vars();
-
-    require_once( ABSPATH . WPINC . '/functions.php' );
-
-    define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
-    require_once( ABSPATH . WPINC . '/version.php' );
-
-    wp_check_php_mysql_versions();
-    wp_load_translations_early();
-
-    // Die with an error message
-    $die  = sprintf(
-        /* translators: %s: wp-config.php */
-        __( "There doesn't seem to be a %s file. I need this before we can get started." ),
-        '<code>.env</code>'
-    ) . '</p>';
-
-    wp_die( $die, __( 'WordPress &rsaquo; Error' ) );
-
-}
-
-$contents = file_get_contents( $env_file );
-$variables = explode( PHP_EOL, $contents );
-$variables = array_diff($variables, [""]);
-
-foreach ( $variables as $variable) {
-    putenv( $variable );
+    foreach ( $variables as $variable) {
+        putenv( $variable );
+    }
 }
 
 function env ( $key, $default = false ) {
